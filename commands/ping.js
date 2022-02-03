e3bf5f5147e1a8a11097113bc1d38558c3760371
PinKevin <emeriokevin2320@gmail.com>
2022-02-04 00:48:34 +0700

    Menambahkan cooldown dan delay ke command ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,23 @@
+const { Collection } = require('discord.js');
+const cdCollection = new Collection;
+
 module.exports = {
     name: 'ping',
     description: 'Command untuk !ping',
     execute(message) {
-        message.reply('pong!');
+        if (cdCollection.has(message.author.id)) {
+            setTimeout(() => {
+                message.reply('tunggu 10 detik lagi sebelum bermain ping-pong dengan bot!').then(msg => {
+                    msg.delete({ timeout: 1000 * 2 });
+                }).catch();
+            }, 1000 * 1.5);
+        }
+        else {
+            message.reply('pong!');
+            cdCollection.set(message.author.id);
+            setTimeout(() => {
+                cdCollection.delete(message.author.id);
+            }, 1000 * 10);
+        }
     },
 };
