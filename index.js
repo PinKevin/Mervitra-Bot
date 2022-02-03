8586387bc9f0d26708c054beeb5780a81138e5ac
PinKevin <emeriokevin2320@gmail.com>
2022-02-03 20:57:36 +0700

    Import command dari file eksternal

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 
 const fs = require('fs');
 const { token } = JSON.parse(fs.readFileSync('./config.json'));
@@ -6,6 +6,14 @@ const { token } = JSON.parse(fs.readFileSync('./config.json'));
 const client = new Client;
 
 const PREFIX = '!';
+
+// impor command dari luar
+const commands = new Collection();
+const files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of files) {
+    const command = require(`./commands/${file}`);
+    commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('Ready!');
