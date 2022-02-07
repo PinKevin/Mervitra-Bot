65c1fbafde9f8e1978ec0f3b43138a9341a79317
PinKevin <emeriokevin2320@gmail.com>
2022-02-07 18:33:54 +0700

    Memodifikasi command ping

--- commands/ping.js
+++ commands/ping.js
@@ -6,13 +6,17 @@ module.exports = {
     description: 'Command untuk !ping',
     execute(message) {
         if (cdCollection.has(message.author.id)) {
+            message.channel.bulkDelete(1);
             message.reply('tunggu 10 detik lagi sebelum bermain ping-pong dengan bot!').then(msg => {
                 msg.delete({ timeout: 1000 * 2 });
             }).catch();
         }
         else {
             setTimeout(() => {
-                message.reply('pong!');
+                message.channel.bulkDelete(1);
+                message.reply('pong!').then(msg => {
+                    msg.delete({ timeout: 1000 * 2 });
+                });
             }, 300);
             cdCollection.set(message.author.id);
             setTimeout(() => {
